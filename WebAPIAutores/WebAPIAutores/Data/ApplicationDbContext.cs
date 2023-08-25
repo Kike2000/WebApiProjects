e0943ab35c0c899f16dc924f33a5da6aec24379a
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-08-24 19:46:40 -0600

    Controllers added

---
+++ WebAPIAutores/WebAPIAutores/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Models;
+
+namespace WebAPIAutores.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Creación de tabla.
+        public DbSet<Autor> Autor { get; set; }
+        public DbSet<Libro> Libro { get; set; }
+
+    }
+}
