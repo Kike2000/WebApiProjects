d43494842eec0ffe540112dda3e75a772f1b929f
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-09-02 15:24:00 -0600

    Validations for API models

--- WebAPIAutores/WebAPIAutores/Models/Autor.cs
+++ WebAPIAutores/WebAPIAutores/Models/Autor.cs
@@ -1,8 +1,11 @@
-﻿namespace WebAPIAutores.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIAutores.Models
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required (ErrorMessage ="El Nombre es requerido.")]
         public string Nombre { get; set; }
         public List<Libro> Libros { get; set; }
     }
