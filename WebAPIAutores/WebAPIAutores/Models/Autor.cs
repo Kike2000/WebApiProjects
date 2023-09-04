dc4673c9f169426d53d126f5d56554b8f5f14a3f
Kike2000 <72471132+Kike2000@users.noreply.github.com>
2023-09-03 18:23:32 -0600

    Validations
    Adding validation key words such as NotMapped, Url, Credit Card, to validate format

--- WebAPIAutores/WebAPIAutores/Models/Autor.cs
+++ WebAPIAutores/WebAPIAutores/Models/Autor.cs
@@ -1,12 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebAPIAutores.Models
 {
     public class Autor
     {
         public int Id { get; set; }
-        [Required (ErrorMessage ="El Nombre es requerido.")]
+        [Required(ErrorMessage = "El Nombre es requerido.")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} caracteres")]
         public string Nombre { get; set; }
+        [Range(18, 120)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string Url { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
